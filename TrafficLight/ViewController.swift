21ad62028b0b904e311cd8b4819900562c1e4c5b
Alex Petrenko <alex.pt@ya.ru>
2024-01-26 16:14:24 +0300

    done

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -1,18 +1,38 @@
 import UIKit
 
+private enum CurrentLight {
+    case red, yellow, green
+}
+
 final class ViewController: UIViewController {
     
-    @IBOutlet weak var redLightView: UIView!
-    @IBOutlet weak var yellowLightView: UIView!
-    @IBOutlet weak var greenLightView: UIView!
+    @IBOutlet private weak var redLightView: UIView!
+    @IBOutlet private weak var yellowLightView: UIView!
+    @IBOutlet private weak var greenLightView: UIView!
+    
+    private var currentLight: CurrentLight = .red
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-    @IBAction func startButtonDidTapped(_ sender: UIButton) {
+    @IBAction private func startButtonDidTapped(_ sender: UIButton) {
+        sender.setTitle("NEXT", for: .normal)
+        switch currentLight {
+        case .red:
+            greenLightView.alpha = lightIsOff
+            redLightView.alpha = lightIsOn
+            currentLight = .yellow
+        case .yellow:
+            redLightView.alpha = lightIsOff
+            yellowLightView.alpha = lightIsOn
+            currentLight = .green
+        case .green:
+            yellowLightView.alpha = lightIsOff
+            greenLightView.alpha = lightIsOn
+            currentLight = .red
+        }
     }
-    
-
 }
-
